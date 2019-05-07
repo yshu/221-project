154f939e17e9c5a33e921466fa307c3e444e178e
Yujie <sunnyyujie@gmail.com>
2019-05-07 01:52:39 +0000

    add baseline linear and dqn models

---
+++ utils/linear.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class LinearSchedule(object):
+    def __init__(self, eps_begin, eps_end, nsteps):
+        self.epsilon        = eps_begin
+        self.eps_begin      = eps_begin
+        self.eps_end        = eps_end
+        self.nsteps         = nsteps
+
+    def update(self, t):
+        """
+        Updates epsilon
+
+        Args:
+            t: int
+            frame number
+        """
+        decay = np.linspace(self.eps_begin, self.eps_end, self.nsteps+1)
+        if t > self.nsteps:
+            self.epsilon = self.eps_end
+        else:
+            self.epsilon = decay[t]
+            
+class LinearExploration(LinearSchedule):
+    def __init__(self, env, eps_begin, eps_end, nsteps):
+        """
+        Args:
+            env: gym environment
+            eps_begin: float
+                initial exploration rate
+            eps_end: float
+                final exploration rate
+            nsteps: int
+                number of steps taken to linearly decay eps_begin to eps_end
+        """
+        self.env = env
+        super(LinearExploration, self).__init__(eps_begin, eps_end, nsteps)
+
+
+    def get_action(self, best_action):
+        """
+        Returns a random action with prob epsilon, otherwise returns the best_action
+        """
+        if np.random.random() < self.epsilon:
+            return self.env.action_space.sample()
+        else:
+            return best_action
